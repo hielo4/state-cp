780ca711815acfa318b4c2c5eff4c00807f2c0a9
ibrahim <djouadaibrahim@gmail.com>
2022-09-06 11:00:05 +0100

    checkpoint done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import image from "./images/pexels.jpeg";
+class App extends Component {
+  state = {
+    person: {
+      fullName: "ibrahim",
+      bio: "iamhere",
+      imgsrc: image,
+      profession: "developper",
+      shows: false,
+    },
+  };
+  handleClick = () => {
+    this.setState((prevState) => {
+      let person = Object.assign({}, prevState.person);
+      person.shows = !person.shows;
+      return { person };
+    });
+  };
+  render() {
+    if (this.state.person.shows === false) {
+      return (
+        <>
+          <h1>nothing to show</h1>
+          <button onClick={this.handleClick}>show profile</button>
+        </>
+      );
+    }
+    return (
+      <>
+        <img src={this.state.person.imgsrc} alt="stock img" />
+        <h1>{this.state.person.fullName}</h1>
+        <h2>{this.state.person.bio}</h2>
+        <h2>{this.state.person.profession} </h2>
+        <button onClick={this.handleClick}>show profile</button>
+      </>
+    );
+  }
 }
 
 export default App;
